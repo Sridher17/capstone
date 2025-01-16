9ab81a44c3e8a02371bc038f9c83a56e2edfe521
Sridher D.C <sridher2817@gmail.com>
2025-01-17 02:20:43 +0530

    update

--- mydress-backend/app.js
+++ mydress-backend/app.js
@@ -22,4 +22,6 @@ app.use('/api/filters', filtersRoute); // Add filters route
 
 // Start the server
 const PORT = process.env.PORT || 5001;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
